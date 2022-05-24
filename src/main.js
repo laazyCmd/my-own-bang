2ce4de205193c4abb353814d6190b011f2cfee6f
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-05-24 20:28:19 +0800

    finished main code (nav, storage) of this extension

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
-const filter = {
-    url: [
-        { hostPrefix: "duckduckgo" }
-    ]
+/** initialization of default values for the first runtime (runs once after install/update) */
+const initializeDefault = ( details ) => {
+    browser.storage.local.get( [ "hostnames", "bangs" ] ).then( items => {
+        if ( items[ "hostnames" ] === undefined ) browser.storage.local.set( { hostnames: [ "duckduckgo" ] } );
+        if ( items[ "bangs" ] === undefined ) browser.storage.local.set( { 
+        bangs: { 
+                libreddit: "https://libredd.it/r/popular/search?q={{s}}",
+                libreddits: "https://libredd.it/r/{{s}}" 
+            } 
+        } );
+    } );
 };
 
-function checkURL( details ) {
-    const regexp = /(%21)(\w+)[+]((\w+|[+])+)/;
-    const search_query = regexp.exec( details.url );
-    if ( search_query[2] === "piped" ) {
-       const url = "https://piped.kavin.rocks/results?search_query={{s}}";
-       browser.tabs.update( { url: url.replace( "{{s}}", search_query[3] ) } );
-    }
+browser.runtime.onInstalled.addListener( initializeDefault );
+
+const checkBang = ( details ) => {
+    const search_query = /(%21)(\w+)[+]((\w+|[+])+)/.exec( details.url ); // get prefix and search query
+    const prefix = search_query[2];
+
+    browser.storage.local.get( "bangs" ).then( data => {
+        if ( data.bangs[ prefix ] ) navigateToBang( data.bangs[ prefix ], search_query[ 3 ] );
+    } );
+};
+
+function navigateToBang( address, query ) {
+    browser.tabs.update( { url: address.replace( "{{s}}", query ) } );
 }
 
-browser.webNavigation.onBeforeNavigate.addListener( checkURL, filter ); 
+browser.webNavigation.onBeforeNavigate.addListener( checkBang, { url: [ { hostPrefix: "duckduckgo" } ] } );
