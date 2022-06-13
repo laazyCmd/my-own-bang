891a6e97a49affdf9b5de020c1a506ac72537c59
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-06-13 15:50:47 +0800

    implemented redirecting to domain name if no query params found

--- src/main.js
+++ src/main.js
@@ -19,17 +19,27 @@ const getHostname = async () => {
     } );
 };
 
-const checkBang = ( details ) => {
-    const search_query = /(%21)(\w+)[+]((\w+|[+])+)/.exec( details.url ); // get prefix and search query
+const checkBang = async ( details ) => {
+    const search_query = /(%21)(\w+)(([+]\w+)*)/.exec( details.url ); // get prefix and search query
     const prefix = search_query[2];
-
-    browser.storage.local.get( "bangs" ).then( data => {
-        if ( data.bangs[ prefix ] ) navigateToBang( data.bangs[ prefix ], search_query[ 3 ] );
-    } );
+    
+    if ( search_query[3] === "" ) {
+        await browser.storage.local.get( "bangs" ).then( data => {
+            if ( data.bangs[ prefix ] ) {
+                const domain_name = /((https|http):\/\/([www]*)(([.]*)(\w+))*)/.exec( data.bangs[ prefix ] );
+                navigateToBang( domain_name[0] );
+            }
+        } );
+    } else {
+        await browser.storage.local.get( "bangs" ).then( data => {
+            if ( data.bangs[ prefix ] ) navigateToBang( data.bangs[ prefix ], search_query[ 3 ].trim() );
+        } );
+    }
 };
 
 const navigateToBang = ( address, query ) => {
-    browser.tabs.update( { url: address.replace( "{{s}}", query ) } );
+    if ( query ) browser.tabs.update( { url: address.replace( "{{s}}", query ) } );
+    else browser.tabs.update( { url: address } );
 }
 
 browser.webNavigation.onBeforeNavigate.addListener( checkBang, { url: [ { hostEquals: getHostname().hostname } ] } );
